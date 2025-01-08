c379d0333b7cae52911a5fada39c9fa8adc55ac6
Abdhilahi Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-01-07 22:06:29 -0700

    Fix Errors

--- server/source/main.go
+++ server/source/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"data-interchange-server/source/network"
 )
 
-const port string = ":8080"
-
-var fileServer http.Handler = http.FileServer(http.Dir("./source"))
-
 func main() {
-	fmt.Println("Serving on Port: 8080")
-	http.ListenAndServe(port, fileServer)
+	network.ServeWebApplication()
 }
